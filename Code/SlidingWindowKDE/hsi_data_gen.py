c5eac8fa32c26168fbbd5bc0ca08d9b1682389f8
Srijan Das <srijan2001kgp@gmail.com>
2025-10-16 20:39:08 +0530

    Modified data gen in SW

--- Code/SlidingWindowKDE/hsi_data_gen.py
+++ Code/SlidingWindowKDE/hsi_data_gen.py
@@ -4,4 +4,5 @@ orig_data=np.load('data/hsi.npy')
 mask=np.load('data/hsi_gt.npy')
 a=orig_data[mask!=0]
 a=a/np.max(a,axis=1,keepdims=True)
+print("Data dimensions :",a.shape)
 np.save('data/hsi_data_points.npy',a)
